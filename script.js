f8cc24a0c2de827728affda18b8d4e5f31869866
syedsufiyann <syedsufiyan4567@gmail.com>
2025-11-09 23:27:28 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+//Arrow Functions
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,7));
+
+// const add = (a,b) => a+b;
+// console.log(add(2,6));
+
+
